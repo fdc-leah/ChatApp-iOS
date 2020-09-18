69e822677f2953967b307a51fbfb576c79b09397
Leah <fdc-leah@gmail.com>
2020-09-18 13:34:30 +0800

    Initial Commit

---
+++ ChatApp-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp-iOS
+//
+//  Created by Leah Joy Ylaya on 9/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
