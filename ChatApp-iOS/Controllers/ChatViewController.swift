fef1fe5c4f865aa2905a2c2342d899de6993f012
Leah <fdc-leah@gmail.com>
2020-09-18 14:09:26 +0800

    prep: Added needed controllers for the application

--- ChatApp-iOS/Controllers/ChatViewController.swift
+++ ChatApp-iOS/Controllers/ChatViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChatViewController.swift
 //  ChatApp-iOS
 //
 //  Created by Leah Joy Ylaya on 9/18/20.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
